804f72e3425f527955e803fe794095c060204a98
Joshar226 <josharaya226@gmail.com>
2024-12-22 16:43:47 -0600

    lastUpdate

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express' 
 import colors from 'colors'
+import cors, {CorsOptions} from 'cors'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec, { swaggerUiOptions } from './config/swagger'
 import router  from './router'
 import db from './config/db'
+import morgan from 'morgan'
 
 // Conectar a base de datos
 export async function connectDB() {
@@ -21,9 +23,22 @@ connectDB()
 // Instancia de express
 const server = express()
 
+//Habilitar conexiones
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de CORS'))
+        }
+    }
+}
+server.use(cors(corsOptions))
+
 // Leer datos de formularios
 server.use(express.json())
 
+server.use(morgan('dev'))
 server.use('/api/products', router)
 
 // Docs
